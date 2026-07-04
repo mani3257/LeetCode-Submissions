f8a6f8b2df4ed0748b840450daaa7e2ea6b10cf4
Gajula Manikumar Reddy <reddygaju57@gmail.com>
2026-07-04 20:58:36 +0530

    Time: 16 ms (39.92%), Space: 47.7 MB (47.97%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -4,18 +4,16 @@ class Solution {
         for(int i:nums){
             mp.put(i,mp.getOrDefault(i,0)+1);
         }
+        Queue<Integer>minHeap=new PriorityQueue<>((a,b)->mp.get(a)-mp.get(b));
+        for(int i:mp.keySet()){
+            minHeap.add(i);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }             
+        }
         int[] ans=new int[k];
         for(int i=0;i<k;i++){
-            int maxFreq=0;
-            int element=0;
-            for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
-                if(entry.getValue()>maxFreq){
-                    maxFreq=entry.getValue();
-                    element=entry.getKey();
-                }
-            }
-            ans[i]=element;
-            mp.remove(element);
+            ans[i]=minHeap.poll();
         }
         return ans;
         
