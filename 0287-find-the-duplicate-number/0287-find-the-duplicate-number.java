5adc772fa6c8fa02ca3c93f20938f18974723b6e
Gajula Manikumar Reddy <reddygaju57@gmail.com>
2026-08-02 22:46:02 +0530

    Time: 4 ms (91.14%), Space: 83 MB (47.47%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,13 +1,22 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Set<Integer> set=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(set.contains(nums[i])){
-                return nums[i];
-            }
-            set.add(nums[i]);
-        }
-        return -1;
+        // Step 1: Initialize slow and fast pointers
+        int slow = nums[0];
+        int fast = nums[0];
         
+        // Step 2: Find the intersection point in the cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        // Step 3: Find the entrance to the cycle (duplicate element)
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
     }
 }
