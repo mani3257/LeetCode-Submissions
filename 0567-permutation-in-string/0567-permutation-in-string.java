b3b78fc528c327d7181ac83e772d078c48d38795
Gajula Manikumar Reddy <reddygaju57@gmail.com>
2026-08-03 22:23:52 +0530

    Time: 6 ms (88.01%), Space: 43.8 MB (85.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,25 +1,23 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
         int n=s1.length();
         int m=s2.length();
-        if(n>m)return false;
+        if(n>m) return false;
         int[] s1freq=new int[26];
+        int[] window=new int[26];
         for(int i=0;i<n;i++){
-            s1freq[s1.charAt(i)-'a']++;
-
+            s1freq[s1.charAt(i)-'a']++;// stored s1 char frequencies
+            window[s2.charAt(i)-'a']++;
         }
-        for(int i=0;i<=m-n;i++){
-            int[] s2freq=new int[26];
-            for(int j=i;j<i+n;j++){
-                s2freq[s2.charAt(j)-'a']++;
-            }
-            if(Arrays.equals(s1freq,s2freq)){
-                return true;
-            }
+        if(Arrays.equals(s1freq,window))return true;
+        for(int i=n;i<m;i++){
+            window[s2.charAt(i)-'a']++;
+            window[s2.charAt(i-n)-'a']--;//shrink from left ,n indexs bedore window size to maintain window size 'n'
+            if(Arrays.equals(s1freq,window)) return true;
 
         }
         return false;
+
         
     }
 }
