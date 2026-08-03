7e8edafe588f04d028dd652a61c2d899208e3371
Gajula Manikumar Reddy <reddygaju57@gmail.com>
2026-08-03 22:56:21 +0530

    Time: 5 ms (97.59%), Space: 43.9 MB (85.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -11,12 +11,12 @@ class Solution {
             
         }
         for(int j=0;j<n;j++){
-            window[s2.charAt(j)-'a']++;
+            window[s2.charAt(j)-'a']++;// stores s1 size chars of  s2 string ('e','i')
         }
-        if(Arrays.equals(s1freq,window))return true;
-        for(int i=1;i<=m-n;i++){
-            window[s2.charAt(i-1)-'a']--;
-            window[s2.charAt(i+n-1)-'a']++;
+        if(Arrays.equals(s1freq,window))return true;//checks the 2 arrays if first window matchs s1
+        for(int i=1;i<=m-n;i++){//check up to last window's first char
+            window[s2.charAt(i-1)-'a']--;//shrink from left, (as we already stores the first 2 chars of s2)remove 1st char from the 2nd char 
+            window[s2.charAt(i+n-1)-'a']++;//add next char to attain window size(windowsize(n)-1) that aminatins window size
             if(Arrays.equals(s1freq,window)) return true;
 
         }
