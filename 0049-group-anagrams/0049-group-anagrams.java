c68a8992abb67a99f04dc7ddbc7865d01b3040ac
Gajula Manikumar Reddy <reddygaju57@gmail.com>
2026-08-16 10:02:17 +0530

    Time: 18 ms (19.94%), Space: 51.2 MB (5.87%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,15 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String,List<String>> mp=new HashMap<>();
-        int n=strs.length;
-        for(int i=0;i<n;i++){
-            char[] c=strs[i].toCharArray();
-            Arrays.sort(c);
-            String key=new String(c);
+        for(String i:strs){
+            int[] f=new int[26];
+            for(char c:i.toCharArray()){
+                f[c-'a']++; // noted freq of each char in evry string
+            }
+            String key=Arrays.toString(f);
             if(!mp.containsKey(key)){
                 mp.put(key,new ArrayList<>());
             }
-            mp.get(key).add(strs[i]);
+            mp.get(key).add(i);
         }
         return new ArrayList<>(mp.values());
         
