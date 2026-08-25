c03eb08ffe950c8ecd60fcd26b3a8fee54fa16e0
Gajula Manikumar Reddy <reddygaju57@gmail.com>
2026-08-25 21:41:26 +0530

    Time: 8 ms (67.57%), Space: 48.1 MB (32.19%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,34 +1,29 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        
-        int max=Integer.MIN_VALUE;
+        int n=piles.length;
+        int max=0;
         for(int i:piles){
-            max=Math.max(i,max);
+            max=Math.max(max,i);
         }
-       
-        //binery search
-       int ans=max;// maximum ele in arr that can be ans though
-       int low=1;// bcz arr:1 to max
-       while(low<=max){
-            int mid=low+(max-low)/2;
-            if(help(piles,h,mid)){
+        int low=1;
+        int ans=0;
+        while(low<=max){
+            int mid=(low+max)/2;
+            if(eatAll(piles,h,mid)){
                 ans=mid;
-                max=mid-1;// if curr mid is max then reduce to find min ele
+                max=mid-1;
             }
             else low=mid+1;
-       } 
-       return ans;
-        
-        
-    }
-        boolean help(int[] piles,int h,int mid){
-            long totalHours=0;
-            for(int i:piles){
-                totalHours+=(i+(long)mid-1)/mid;
-
-            }
-            return totalHours<=h;
         }
+        return ans;
         
-    
+      
+    }
+    boolean eatAll(int[] piles,int h,int mid){
+        long totalHours=0;
+        for(int i=0;i<piles.length;i++){
+            totalHours+=(piles[i]+(long)mid-1)/mid;
+        }
+        return totalHours<=h;
+    }
 }
