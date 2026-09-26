60bdc92d6be5e8ec62bd64d6aabaf476c0db7d08
Gajula Manikumar Reddy <reddygaju57@gmail.com>
2026-09-26 18:56:35 +0530

    Time: 0 ms (100%), Space: 48.1 MB (73.78%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,10 @@
 class Solution {
     public void reverseString(char[] s) {
-        // use 2 pointer t.c=O(n) s.c=O(1)
-        int l=0,r=s.length-1;
-        while(l<r){
-            char t=s[l];
-            s[l]=s[r];
-            s[r]=t;
-            l++;
-            r--;
+        int n=s.length;
+        for(int i=0;i<n/2;i++){
+            char t=s[i];
+            s[i]=s[n-1-i];
+            s[n-1-i]=t;
         }
         
     }
